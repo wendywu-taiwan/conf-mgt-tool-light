ece2b74f86e131c43094d2898ba05ee7722848d4
wendy.wu@audatex.com <wendy.wu@audatex.com>
2019-02-13 16:47:37 +0800

    AXNASEAN-2873 test for restart all scheduler in app.config

--- RulesetComparer/apps.py
+++ RulesetComparer/apps.py
@@ -1,6 +1,7 @@
-import os
+import os, sys
 from django.apps import AppConfig
 from RulesetComparer.utils.logger import *
+from RulesetComparer.services.services import restart_all_scheduler
 
 
 class RulesetcomparerConfig(AppConfig):
@@ -10,3 +11,5 @@ class RulesetcomparerConfig(AppConfig):
         # django will run twice when run server
         if os.environ.get('RUN_MAIN', None) == 'true':
             return
+        if 'manage.py' not in sys.argv:
+            restart_all_scheduler()
