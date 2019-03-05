5da81b4933cc8c071644244848ba5bdea94cf799
wendy.wu@audatex.com <wendy.wu@audatex.com>
2019-03-05 16:27:25 +0800

    fix create task time not valid bug

--- RulesetComparer/dataModel/dataParser/createReportSchedulerTaskParser.py
+++ RulesetComparer/dataModel/dataParser/createReportSchedulerTaskParser.py
@@ -35,35 +35,77 @@ class CreateReportSchedulerTaskParser:
             error_log(traceback.format_exc())
             raise e
 
-    @staticmethod
-    def parse_utc_time(start_time):
+    def get_compare_time(self, start_date_time):
         try:
             time_zone = config.TIME_ZONE.get('asia_taipei')
-            time_format = config.TIME_FORMAT.get('year_month_date_hour_minute_second')
+            frontend_time_format = config.TIME_FORMAT.get('year_month_date_hour_minute_second')
 
-            # parse str time to date time
-            date_time = timeUtil.time_to_date_time(start_time, time_format)
-            # parse local date time to utc time
-            utc_time = timeUtil.local_time_to_utc(date_time, time_zone)
+            date_time = timeUtil.time_to_date_time(start_date_time, frontend_time_format)
+            local_date_time = timeUtil.date_time_change_time_zone(date_time, time_zone)
+            current_date_time = timeUtil.get_current_date_time()
+
+            # compare local time and current time
+            if local_date_time > current_date_time:
+                result_time = local_date_time
+            else:
+                local_date_time = local_date_time.replace(year=current_date_time.year)
+                local_date_time = local_date_time.replace(month=current_date_time.month)
+                if self.local_date_time_valid(local_date_time, current_date_time):
+                    local_date_time = local_date_time.replace(day=current_date_time.day)
+                else:
+                    local_date_time = local_date_time.replace(day=current_date_time.day + 1)
+                result_time = local_date_time
+
+            naive_result_time = datetime(result_time.year, result_time.month, result_time.day,
+                                         result_time.hour, result_time.minute, result_time.second)
+            return naive_result_time
+
+        except Exception as e:
+            traceback.print_exc()
+            error_log(traceback.format_exc())
+            raise e
+
+    def parse_utc_time(self, start_time):
+        try:
+            time_zone = config.TIME_ZONE.get('asia_taipei')
+            naive_result_time = self.get_compare_time(start_time)
+
+            # transfer time zone to utc
+            utc_time = timeUtil.local_time_to_utc(naive_result_time, time_zone)
             return utc_time
+
+        except Exception as e:
+            traceback.print_exc()
+            error_log(traceback.format_exc())
+            raise e
+
+    def parse_local_time(self, start_time):
+        try:
+            return self.get_compare_time(start_time)
         except Exception as e:
             traceback.print_exc()
             error_log(traceback.format_exc())
             raise e
 
     @staticmethod
-    def parse_local_time(start_time):
+    def local_date_time_valid(local_date_time, current_date_time):
         try:
-            time_zone = config.TIME_ZONE.get('asia_taipei')
-            time_format = config.TIME_FORMAT.get('year_month_date_hour_minute_second')
-
-            # parse str time to date time
-            date_time = timeUtil.time_to_date_time(start_time, time_format)
-            # parse local date time to utc time
-            local_date_time = timeUtil.date_time_change_time_zone(date_time, time_zone)
-            naive_time = datetime(local_date_time.year, local_date_time.month, local_date_time.day,
-                                  local_date_time.hour, local_date_time.minute, local_date_time.second)
-            return naive_time
+            if local_date_time.hour < current_date_time.hour:
+                return False
+            elif local_date_time.hour > current_date_time.hour:
+                return True
+            # if same hour, compare minutes
+            else:
+                if local_date_time.minute < current_date_time.minute:
+                    return False
+                elif local_date_time.minute > current_date_time.minute:
+                    return True
+                # if same minute, compare second
+                else:
+                    if local_date_time.second > current_date_time.second:
+                        return True
+                    else:
+                        return False
         except Exception as e:
             traceback.print_exc()
             error_log(traceback.format_exc())
