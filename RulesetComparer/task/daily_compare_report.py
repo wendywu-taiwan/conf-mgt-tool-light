6f59800292779efcfb10cd39a1189524d42768ea
wendy.wu@audatex.com <wendy.wu@audatex.com>
2019-11-18 13:49:26 +0800

    archive attached ruleset compare report

--- RulesetComparer/task/daily_compare_report.py
+++ RulesetComparer/task/daily_compare_report.py
@@ -1,5 +1,5 @@
 import traceback
-
+from shutil import copyfile
 from ConfigManageTool.settings import CURRENT_TIME_ZONE
 from RulesetComparer.task.compare_ruleset_list import CompareRuleListTask
 from RulesetComparer.utils.logger import *
@@ -10,6 +10,9 @@ from django.template.loader import render_to_string
 from RulesetComparer.models import Environment, ReportSchedulerInfo
 from RulesetComparer.date_model.json_builder.compare_report_info import CompareReportInfoBuilder
 from RulesetComparer.task.base_scheduler import BaseSchedulerTask
+from common.properties.config import RULESET_ZIP_PATH, RULESET_ZIP_FILE_PATH, \
+    RULESET_COMPARE_RESULT_ZIP_RESOURCE, RULESET_COMPARE_RESULT_HTML, RULESET_COMPARE_RESULT_FOLDER_PATH
+from RulesetComparer.utils.fileManager import create_folder, archive_file_with_arcname, clear_folder
 
 
 class DailyCompareReportTask(BaseSchedulerTask):
@@ -56,11 +59,22 @@ class DailyCompareReportTask(BaseSchedulerTask):
                 html_content = render_to_string('compare_info_mail_content.html', content_json)
 
                 if content_json[COMPARE_RESULT_HAS_CHANGES]:
-                    # add attachment
-                    file_path = fileManager.get_compare_result_full_file_name("_html", task.compare_hash_key)
-                    file_name = current_time + "_" + self.base_env.name + "_" + self.compare_env.name + "_" + country.name + "_" + "compare_report.html"
+                    # copy report to single folder
+                    copy_path = RULESET_COMPARE_RESULT_FOLDER_PATH % str(task.compare_hash_key)
+                    create_folder(copy_path)
+
+                    resource_full_path = RULESET_COMPARE_RESULT_HTML % task.compare_hash_key
+                    copy_full_path = RULESET_COMPARE_RESULT_ZIP_RESOURCE % (task.compare_hash_key, task.compare_hash_key)
+                    copyfile(resource_full_path, copy_full_path)
+
+                    # archive single folder report
+                    zip_file_path = RULESET_ZIP_FILE_PATH % task.compare_hash_key
+                    archive_file_with_arcname(copy_path, RULESET_ZIP_PATH, zip_file_path)
+                    clear_folder(copy_path)
+
+                    file_name = current_time + "_" + self.base_env.name + "_" + self.compare_env.name + "_" + country.name + "_" + "compare_report.zip"
                     application = "text"
-                    self.mail_sender.add_attachment(file_path, file_name, application)
+                    self.mail_sender.add_attachment(zip_file_path, file_name, application)
 
                 self.mail_sender.set_receiver(self.mail_list)
                 self.mail_sender.compose_msg(subject, None, html_content)
--- common/properties/config.py
+++ common/properties/config.py
@@ -5,3 +5,11 @@ PRELOAD_DATA_PATH = settings.BASE_DIR + "/permission/properties/" + settings.PRE
 AUTH_DATA_PATH = settings.BASE_DIR + "/permission/properties/" + settings.AUTH_DATA
 CONF_DATA_PATH = settings.BASE_DIR + "/common/properties/conf/"
 USER_ROLE_PERMISSION_DATA_PATH = settings.BASE_DIR + "/permission/properties/" + settings.USER_ROLE_PERMISSION_DATA
+RULESET_ZIP_PATH = settings.BASE_DIR + "/RulesetComparer/rulesets/zip/"
+RULESET_ZIP_FILE_PATH = settings.BASE_DIR + "/RulesetComparer/rulesets/zip/%s.zip"
+
+RULESET_COMPARE_RESULT_PATH = settings.BASE_DIR + "/RulesetComparer/compare_result/"
+RULESET_COMPARE_RESULT_FOLDER_PATH = settings.BASE_DIR + "/RulesetComparer/compare_result/%s/"
+RULESET_COMPARE_RESULT_HTML = RULESET_COMPARE_RESULT_PATH + "%s.html"
+RULESET_COMPARE_RESULT_JSON = RULESET_COMPARE_RESULT_PATH + "%s.json"
+RULESET_COMPARE_RESULT_ZIP_RESOURCE = RULESET_COMPARE_RESULT_PATH + "%s/%s.html"
