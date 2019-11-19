81cb1efb535be3c506cc1ff7898cf1212baa261b
wendy.wu@audatex.com <wendy.wu@audatex.com>
2019-11-19 15:38:24 +0800

    add permission check new error handle - no available data

--- common/views.py
+++ common/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from RulesetComparer.utils.logger import error_log
 from common.data_object.error.error import PermissionDeniedError, B2BRulesetNotFoundError, \
-    SharedStorageFolderNotFoundError, SharedStorageFTPServerConnectFailError
+    SharedStorageFolderNotFoundError, SharedStorageFTPServerConnectFailError, NoAvailableDataError
 from common.data_object.error.message import PERMISSION_DENIED_MESSAGE, RULESET_NOT_FOUND_MESSAGE, \
-    FOLDER_NOT_EXIST_MESSAGE, CONNECT_TO_FTP_SERVER_FAIL_MESSAGE
+    FOLDER_NOT_EXIST_MESSAGE, CONNECT_TO_FTP_SERVER_FAIL_MESSAGE, NO_AVAILABLE_DATA_MESSAGE
 from common.data_object.error.status import RULESET_NOT_FOUND, PERMISSION_DENIED, FOLDER_NOT_EXIST, \
-    CANT_CONNECT_FTP_SERVER
+    CANT_CONNECT_FTP_SERVER, NO_AVAILABLE_DATA
 from common.data_object.json_builder.response import ResponseBuilder
 
 
@@ -16,6 +16,9 @@ def page_permission_check(request, check_visibility, get_visible_data, executor)
     try:
         check_visibility()
         return executor(get_visible_data())
+    except NoAvailableDataError:
+        data = ResponseBuilder(status_code=NO_AVAILABLE_DATA, message=NO_AVAILABLE_DATA_MESSAGE).get_data()
+        return JsonResponse(data)
     except PermissionDeniedError:
         data = ResponseBuilder(status_code=PERMISSION_DENIED, message=PERMISSION_DENIED_MESSAGE).get_data()
         return render(request, "permission_denied.html", data)
