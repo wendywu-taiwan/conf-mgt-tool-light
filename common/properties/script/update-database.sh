06eb81ffbe5a4330d28789f32d16e38f38c5139d
wendy.wu@audatex.com <wendy.wu@audatex.com>
2019-12-10 14:28:04 +0800

    add shared storage migrate script

--- common/properties/script/update-database.sh
+++ common/properties/script/update-database.sh
@@ -6,7 +6,9 @@ pip install -r requirements.txt
 python3 manage.py makemigrations permission
 python3 manage.py makemigrations common
 python3 manage.py makemigrations RulesetComparer
+python3 manage.py makemigrations shared_storage
 python3 manage.py migrate permission
 python3 manage.py migrate common
 python3 manage.py migrate RulesetComparer
+python3 manage.py migrate shared_storage
 python3 manage.py migrate
