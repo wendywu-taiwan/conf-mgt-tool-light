12a86876a92f2b4f23df4f374c67706318676089
wendy.wu@audatex.com <wendy.wu@audatex.com>
2019-12-31 11:03:26 +0800

    AXNASEAN-3959 prepare GAS instance configuration data

---
+++ common/properties/conf/gas_conf.py
@@ -0,0 +1,9 @@
+from common.properties.region_setting import REGION_GAS
+
+CURRENT_REGION = REGION_GAS
+STATIC_URL = "/gas/static/"
+URL_PRE_PATH = "gas/"
+PRELOAD_DATA = "preload_data_gas.json"
+AUTH_DATA = "auth_data_gas.json"
+USER_ROLE_PERMISSION_DATA = "user_role_permission_gas.json"
+
