de0d0185e879cfba2b80ad2da20a51c200a8ee60
wendy.wu@audatex.com <wendy.wu@audatex.com>
2020-01-21 16:58:04 +0800

    Add core instance setting

---
+++ common/properties/conf/core_conf.py
@@ -0,0 +1,9 @@
+from common.properties.region_setting import REGION_CORE
+
+CURRENT_REGION = REGION_CORE
+STATIC_URL = "/core/static/"
+URL_PRE_PATH = "core/"
+PRELOAD_DATA = "preload_data_core.json"
+AUTH_DATA = "auth_data_core.json"
+USER_ROLE_PERMISSION_DATA = "user_role_permission_core.json"
+
---
+++ common/properties/script/run-server-core.sh
@@ -0,0 +1,7 @@
+cd ..
+cd ..
+cd ..
+. venv/bin/activate
+python manage.py init_conf core
+# python manage.py runserver 00.0.0.0:8024
+nohup python manage.py runserver 00.0.0.0:8024 &
